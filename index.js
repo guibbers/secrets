f757b0d358727998dbf0beadc44e141e4c2db291
Guilherme Torres <guiatorres8@gmail.com>
2023-09-06 22:32:29 -0300

    finish project

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res)=>{
+    try {
+        const result = await axios.get('https://secrets-api.appbrewery.com/random');
+        res.render('index', { 
+            secret: result.data.secret, 
+            user: result.data.username
+        });
+    } catch(error) {
+        console.log(error.response.data);
+        res.status(500)
+    }
+    
+});
+
+app.listen(port, ()=>{
+    console.log('Server Running on Port: ' + port);
+});
